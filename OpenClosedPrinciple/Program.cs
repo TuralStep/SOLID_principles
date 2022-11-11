d6c32a063a54990e7d5be6c47007c27890a402c1
TuralStep <turalozel@gmail.com>
2022-11-11 16:19:55 +0400

    Add project files.

---
+++ OpenClosedPrinciple/Program.cs
@@ -0,0 +1,11 @@
+﻿#region OCP_before
+
+
+
+#endregion
+
+#region OCP_AFTER
+
+
+
+#endregion
---
+++ UML_2/Program.cs
@@ -0,0 +1,41 @@
+﻿#region SRP_before
+
+class Employee
+{
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+
+
+    public void PrintTimeSheetReport()
+    {
+        // do smth...
+    }
+
+}
+
+#endregion
+
+#region SRP_AFTER
+
+class TimeSheetReport
+{
+
+    public void Print(Employee2 employee)
+    {
+        // do smth...
+    }
+
+}
+
+class Employee2
+{
+
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+
+}
+
+#endregion
